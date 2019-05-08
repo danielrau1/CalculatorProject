869337dcdbadff4a0fd48f8bf2ed3f391e78ad99
Daniel Rau <rauddaniel@yahoo.com>
2019-05-08 16:27:02 -0400

    the calculator includes the nth root

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,11 +12,11 @@ export class HomeComponent implements OnInit {
     val1: string;
     val2: string;
     result: number;
-    operations = ["Add", "Subtract", "Multiply", "Divide", "Power of"];
+    operations = ["Add", "Subtract", "Multiply", "Divide", "Power of", "Nth root"];
     operator = 0;
 
     changeOperator(e) {
-        let picker = <ListPicker>e.object;
+        const picker = <ListPicker>e.object;
         this.operator = picker.selectedIndex;
         this.calculate(null);
     }
@@ -44,6 +44,9 @@ export class HomeComponent implements OnInit {
                 break;
             case 4:
                 this.result = Math.pow(Number(this.val1), Number(this.val2));
+                break;
+            case 5:
+                this.result = Math.pow(Number(this.val1), 1 / Number(this.val2));
                 break;
         }
     }
